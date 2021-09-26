8f672a8d95b3339642e5cdc953f7b478af538a17
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-09-26 16:48:09 +0200

    Add comments to tricks

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Comment;
 use App\Entity\Trick;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -25,10 +26,9 @@ class AppFixtures extends Fixture
                 ->setName("Catégorie {$c}")
                 ->setSlug(strtolower($this->slugger->slug($category->getName())))
             ;
-
             $manager->persist($category);
 
-            for ($t = 1; $t < mt_rand(3, 10); ++$t) {
+            for ($t = 1; $t <= mt_rand(3, 10); ++$t) {
                 $trick = new Trick();
                 $trick
                     ->setName("Trick numéro {$t} - Catégorie {$c}")
@@ -37,8 +37,17 @@ class AppFixtures extends Fixture
                     ->setCreatedAt(new \DateTime())
                     ->setCategory($category)
                 ;
-
                 $manager->persist($trick);
+
+                for ($i = 1; $i <= mt_rand(2, 5); ++$i) {
+                    $comment = new Comment();
+                    $comment
+                        ->setContent("Commentaire {$i} Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero praesentium, voluptatum expedita incidunt deserunt nisi assumenda quos tenetur fugit architecto.")
+                        ->setCreatedAt(new \DateTime())
+                        ->setTrick($trick)
+                    ;
+                    $manager->persist($comment);
+                }
             }
         }
 
