8ba87ebe1f55127ca514abc77fcd77651c6d4a63
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-10-08 16:34:45 +0200

    trick add form creation

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
@@ -21,8 +26,35 @@ class TrickController extends AbstractController
     /**
      * @Route("/trick/add", name="trick_add")
      */
-    public function add()
+    public function add(FormFactoryInterface $factory)
     {
-        return $this->render('trick/add.html.twig');
+        $builder = $factory->createBuilder();
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Tapez le nom du trick',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Décrivez le trick en quelques mots',
+                ],
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'placeholder' => '-- Choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+        ;
+
+        $form = $builder->getForm();
+        $formView = $form->createView();
+
+        return $this->render('trick/add.html.twig', [
+            'formView' => $formView,
+        ]);
     }
 }
