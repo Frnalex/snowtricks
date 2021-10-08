50a2bd8d76924460e33649b515da402a6eb4858f
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-10-08 17:20:40 +0200

    trick add refactoring

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,15 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Trick;
+use App\Form\TrickType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -30,34 +25,9 @@ class TrickController extends AbstractController
     /**
      * @Route("/trick/add", name="trick_add")
      */
-    public function add(FormFactoryInterface $factory, Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    public function add(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
     {
-        $builder = $factory->createBuilder(FormType::class, null, [
-            'data_class' => Trick::class,
-        ]);
-
-        $builder
-            ->add('name', TextType::class, [
-                'label' => 'Nom',
-                'attr' => [
-                    'placeholder' => 'Tapez le nom du trick',
-                ],
-            ])
-            ->add('description', TextareaType::class, [
-                'label' => 'Description',
-                'attr' => [
-                    'placeholder' => 'Décrivez le trick en quelques mots',
-                ],
-            ])
-            ->add('category', EntityType::class, [
-                'label' => 'Catégorie',
-                'placeholder' => '-- Choisir une catégorie --',
-                'class' => Category::class,
-                'choice_label' => 'name',
-            ])
-        ;
-
-        $form = $builder->getForm();
+        $form = $this->createForm(TrickType::class);
 
         $form->handleRequest($request);
 
---
+++ src/Form/TrickType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Tapez le nom du trick',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Décrivez le trick en quelques mots',
+                ],
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'placeholder' => '-- Choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
