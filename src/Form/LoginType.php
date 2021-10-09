60ba8e8aca514a3e94eb720718b369c722a3d1ce
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-10-09 17:14:20 +0200

    complete LoginFormAuthenticator and get errors

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +13,6 @@ class LoginType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, [
-                'label' => "Nom d'utilisateur",
-                'attr' => [
-                    'placeholder' => "Nom d'utilisateur",
-                ],
-                'required' => false,
-            ])
             ->add('email', EmailType::class, [
                 'label' => 'Email',
                 'attr' => [
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -6,29 +6,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
     public function supports(Request $request): ?bool
     {
-        // TODO: Implement supports() method.
+        return 'security_login' === $request->attributes->get('_route') && $request->isMethod('POST');
     }
 
     public function authenticate(Request $request): PassportInterface
     {
-        // TODO: Implement authenticate() method.
+        $credentials = $request->request->get('login');
+
+        return new Passport(
+            new UserBadge($credentials['email']),
+            new PasswordCredentials($credentials['password'])
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        dd('Success');
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        $login = $request->request->get('login');
+        $request->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
+        $request->attributes->set(Security::LAST_USERNAME, $login['email']);
+
+        return null;
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
