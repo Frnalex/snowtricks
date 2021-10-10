cbe5887189dc27f9824781aca54ddb075c81af17
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-10-10 17:49:03 +0200

    Association between User and Comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -35,6 +35,12 @@ class Comment
      */
     private $trick;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
     public function __construct()
     {
         $this->createdAt = new DateTime();
@@ -70,6 +76,18 @@ class Comment
     public function setTrick(?Trick $trick): self
     {
         $this->trick = $trick;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
