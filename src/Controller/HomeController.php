a24673e96a356583c4924c93210c7048ebe9c1dc
Alexandre Fournou <alexandre.fournou@gmail.com>
2021-10-31 01:00:16 +0200

    [Feature] Load more tricks button

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,9 +13,23 @@ class HomeController extends AbstractController
      */
     public function homepage(TrickRepository $trickRepository)
     {
-        $tricks = $trickRepository->findBy([], ['createdAt' => 'DESC']);
+        $tricks = $trickRepository->findBy([], ['createdAt' => 'DESC'], 6);
+        $total = $trickRepository->count([]);
 
         return $this->render('home.html.twig', [
+            'tricks' => $tricks,
+            'total' => $total
+        ]);
+    }
+
+    /**
+     * @Route("/{offset}", name="loadMoreTricks", requirements={"offset": "\d+"})
+     */
+    public function loadMoreTricks(TrickRepository $trickRepository, $offset = 6)
+    {
+        $tricks = $trickRepository->findBy([], ['createdAt' => 'DESC'], 6, $offset);
+
+        return $this->render('trickList.html.twig', [
             'tricks' => $tricks,
         ]);
     }
